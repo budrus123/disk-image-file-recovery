85276f46ff2f5da5e2de7663b2f6f4ce25f5d392
Mahmoud Abdelkareem <budrus123@gmail.com>
2020-11-21 11:03:48 -0600

    before final refactor

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 
 ################## File headers and footers ##################
 
-disk_image_name = 'Project2.dd'
+disk_image_name = 'Project2Updated.dd'
 
 pdf_header_string = '25504446'
 pdf_footer_string = '0A2525454F46'
@@ -17,15 +17,29 @@ bmp_header_string = '424D'
 
 docx_header_string = '504B030414000600'
 docx_footer_string = '504B0506'
-
+''
 AVI_header_string = '52494646'
 
 gif_header_string = '474946383961'
-gif_footer_string = '003B'
+gif_footer_string = '003B000000000000'
+
+png_header_string = '89504E470D0A1A0A'   ##-- 2 HITS - DECK 6/SLIDE 27 HAS INFO; 0X0488E29 (DOES NOT START FROM THE LEFT) AND 0X04AA000(STARTS FROM THE LEFT)
+png_footer_string = '49454E44AE426082'   ##-- 2 HITS - DECK 6/SLIDE 27 HAS INFO; 0X04A7284 AND 0X04E1A73
+
+jpg_header_string = 'FFD8FFE0'
+jpg_footer_string = 'FFD900000000'
+jpg_header_string_ffdb = 'FFD8FFDB'
 
 
-supported_types = ['MPG', 'PDF','BMP','GIF','ZIP','JPG','DOCX','AVI','PNG']
-supported_types = ['PDF','AVI']
+supported_types = ['MPG', 'PDF','BMP','GIF','JPG','DOCX','AVI','PNG']
+supported_types = ['PDF','AVI','DOCX','GIF']
+supported_types = ['GIF']
+# supported_types = ['PNG']
+# supported_types = ['JPG','JPG_FFDB']
+
+# GIF, DOCX -> Mahmoud
+# JPG, PNG -> Janet
+# BMP, ZIP -> WARREN
 
 """
 Variables for the program run:
@@ -63,12 +77,24 @@ start at sectors (512) and returns the
 valid header locations only
 """
 def remove_illegal_headers(headers):
-    valid_headers = headers
+    valid_headers = []
     for occurrence in headers:
         o_location = get_offset_for_location(occurrence)
-        if o_location % 512 != 0:
-            valid_headers.remove(occurrence)
+        if o_location % 512 == 0:
+            valid_headers.append(occurrence)
     return valid_headers
+
+
+"""
+Function that removes footers that appear
+before any header
+"""
+def remove_illegal_footers(footers, headers):
+    valid_footers = []
+    for occurrence in footers:
+        if occurrence > headers[0]:
+            valid_footers.append(occurrence)
+    return valid_footers
 
 
 """
@@ -103,21 +129,33 @@ def init_signatures():
         signature['footer'] = ''
         signatures[file_type] = signature
 
-    #PDF
-    signatures['PDF']['header'] = pdf_header_string
-    signatures['PDF']['footer'] = pdf_footer_string
-
-    #DOCX
+    # #PDF
+    # signatures['PDF']['header'] = pdf_header_string
+    # signatures['PDF']['footer'] = pdf_footer_string
+    #
+    # # DOCX
     # signatures['DOCX']['header'] = docx_header_string
     # signatures['DOCX']['footer'] = docx_footer_string
-
-    #AVI
-    signatures['AVI']['header'] = AVI_header_string
-    signatures['AVI']['footer'] = ''
+    #
+    # #AVI
+    # signatures['AVI']['header'] = AVI_header_string
+    # signatures['AVI']['footer'] = ''
 
     #GIF
-    # signatures['GIF']['header'] = gif_header_string
-    # signatures['GIF']['footer'] = gif_footer_string
+    signatures['GIF']['header'] = gif_header_string
+    signatures['GIF']['footer'] = gif_footer_string
+
+    #PNG
+    # signatures['PNG']['header'] = png_header_string
+    # signatures['PNG']['footer'] = png_footer_string
+
+    # JPG
+    # signatures['JPG']['header'] = jpg_header_string
+    # signatures['JPG']['footer'] = jpg_footer_string
+    #
+    # signatures['JPG_FFDB']['header'] = jpg_header_string_ffdb
+    # signatures['JPG_FFDB']['footer'] = jpg_footer_string
+
 
 
 """
@@ -128,8 +166,8 @@ global variable.
 def open_file(path):
     global image_bytes, bytes_hex
     with open(path, 'rb') as f:
-        image_bytes = f.read()
-        bytes_hex = image_bytes.hex().upper()
+        image_bytes = f.read() #bytes
+        bytes_hex = image_bytes.hex().upper() #string
 
 
 
@@ -197,7 +235,6 @@ in the header
 def handle_avi(headers):
 
     header_count = len(headers)
-    print(headers)
 
     """
     if no headers found, return.
@@ -224,6 +261,212 @@ def handle_avi(headers):
         f.close()
 
 
+
+def handle_docx(headers, footers):
+    header_count = len(headers)
+    footer_count = len(footers)
+
+    file_start = -1
+    file_end = -1
+    footer_iterator = -1
+
+    """
+    if no headers found, return.
+    """
+    if header_count == 0:
+        return
+
+    """
+    Loop the header locations
+    """
+    for index in range(header_count):
+
+        """
+        The start of the file, is always the current
+        header in the sequence
+        """
+        file_start = get_offset_for_location(headers[index])
+
+        """
+        If last header, then for sure the footer for this file,
+        is the last possible footer. End = last footer.
+
+        If not last header, then the footer for this file,
+        will be the last footer with an offset before the current header.
+        """
+        if index == (header_count - 1):
+            # add 22 for file footer length
+            file_end = get_offset_for_location(footers[footer_count - 1]) + 22
+        else:
+            footer_iterator = 0
+            while footers[footer_iterator] < headers[index + 1]:
+                footer_iterator += 1
+            file_end = get_offset_for_location(footers[footer_iterator - 1]) + 22
+
+        """
+        Carve bytes out from the image bytes
+        """
+        file_bytes = image_bytes[file_start:file_end]
+        f = open('docx_' + str(index) + '.docx', 'wb')
+        f.write(file_bytes)
+        f.close()
+
+
+##############START OF PNG HANDLE #####################
+
+def handle_png(headers, footers):
+    header_count = len(headers)
+    footer_count = len(footers)
+
+    file_start = -1
+    file_end = -1
+    footer_iterator = -1
+
+    """
+    if no headers found, return.
+    """
+    if header_count == 0:
+        return
+
+    """
+    Loop the header locations
+    """
+    for index in range(header_count):
+
+        """
+        The start of the file, is always the current
+        header in the sequence
+        """
+        file_start = get_offset_for_location(headers[index])
+
+        """
+        If last header, then for sure the footer for this file,
+        is the last possible footer. End = last footer.
+
+        If not last header, then the footer for this file,
+        will be the last footer with an offset before the current header.
+        """
+        if index == (header_count):
+            file_end = get_offset_for_location(footers[footer_count])+ 8
+        else:
+            footer_iterator = 0
+            while footers[footer_iterator] < headers[index]:
+                footer_iterator += 1
+            file_end = get_offset_for_location(footers[footer_iterator]) + 8
+            #TODO: add the bytes for the footer
+
+        """
+        Carve bytes out from the image bytes
+        """
+        file_bytes = image_bytes[file_start:file_end]
+        f = open('png_' + str(index) + '.png', 'wb')
+        f.write(file_bytes)
+        f.close()
+
+
+##############END OF PNG#####################
+
+def handle_gif(headers, footers):
+    header_count = len(headers)
+    footer_count = len(footers)
+
+    file_start = -1
+    file_end = -1
+    footer_iterator = -1
+
+    """
+    if no headers found, return.
+    """
+    if header_count == 0:
+        return
+
+    """
+    Loop the header locations
+    """
+    for index in range(header_count):
+
+        """
+        The start of the file, is always the current
+        header in the sequence
+        """
+        file_start = get_offset_for_location(headers[index])
+
+        """
+        If last header, then for sure the footer for this file,
+        is the last possible footer. End = last footer.
+
+        If not last header, then the footer for this file,
+        will be the last footer with an offset before the current header.
+        """
+        footer_iterator = 0
+        while footers[footer_iterator] < headers[index]:
+            footer_iterator += 1
+        file_end = get_offset_for_location(footers[footer_iterator]) + 2
+
+
+        """
+        Carve bytes out from the image bytes
+        """
+
+        file_bytes = image_bytes[file_start:file_end]
+        f = open("gif_new_" + str(index) + '.gif', 'wb')
+        f.write(file_bytes)
+        f.close()
+
+##############START OF JPG HANDLE #####################
+
+def handle_jpg(headers, footers, file_type):
+    header_count = len(headers)
+    footer_count = len(footers)
+
+    file_start = -1
+    file_end = -1
+    footer_iterator = -1
+
+    """
+    if no headers found, return.
+    """
+    if header_count == 0:
+        return
+
+    """
+    Loop the header locations
+    """
+    for index in range(header_count):
+
+        """
+        The start of the file, is always the current
+        header in the sequence
+        """
+        file_start = get_offset_for_location(headers[index])
+
+        """
+        If last header, then for sure the footer for this file,
+        is the last possible footer. End = last footer.
+
+        If not last header, then the footer for this file,
+        will be the last footer with an offset before the current header.
+        """
+        footer_iterator = 0
+        while footers[footer_iterator] < headers[index]:
+            footer_iterator += 1
+        file_end = get_offset_for_location(footers[footer_iterator]) + 2
+
+
+        """
+        Carve bytes out from the image bytes
+        """
+
+        file_bytes = image_bytes[file_start:file_end]
+        f = open(file_type +"_" + str(index) + '.jpg', 'wb')
+        f.write(file_bytes)
+        f.close()
+
+
+
+        #############END OF JPG#####################
+
+
 ################## Main ######################################
 
 """
@@ -231,6 +474,7 @@ Initialization signatures and open the file
 """
 init_signatures()
 open_file(disk_image_name)
+
 
 """
 Loop all the signatures in the signatures dictionary and one by one do:
@@ -240,6 +484,7 @@ Loop all the signatures in the signatures dictionary and one by one do:
     4. if file has footers, find the location of the footers
     5. send header locations, footer locations to appropriate handling function
 """
+
 for sig in signatures:
     sig_header = signatures[sig]['header']
     sig_footer = signatures[sig]['footer']
@@ -249,8 +494,19 @@ for sig in signatures:
 
     if sig_footer != '':
         footer_occ = find_all_occurrences(sig_footer, bytes_hex)
+        footer_occ = remove_illegal_footers(footer_occ, header_occ)
 
     if sig == 'PDF':
         handle_pdf(header_occ, footer_occ)
     elif sig == 'AVI':
         handle_avi(header_occ)
+    elif sig == 'DOCX':
+        handle_docx(header_occ,footer_occ)
+    elif sig == 'GIF':
+        handle_gif(header_occ,footer_occ)
+    elif sig == 'PNG':
+        handle_png(header_occ,footer_occ)
+    elif sig == 'JPG':
+        handle_jpg(header_occ, footer_occ, 'jpg')
+    elif sig == 'JPG_FFDB':
+        handle_jpg(header_occ, footer_occ, 'jpg_ffdb')
